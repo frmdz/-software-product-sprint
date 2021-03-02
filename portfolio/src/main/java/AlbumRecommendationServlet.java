03561f40c1735fc52e3b185b54ab71d96dcfa15d
Francisco Mendoza <33354499+frmdz@users.noreply.github.com>
2021-03-02 18:00:28 +0000

    the servlet now returns a json string and the site now chooses a random element from that json

--- portfolio/src/main/java/AlbumRecommendationServlet.java
+++ portfolio/src/main/java/AlbumRecommendationServlet.java
@@ -13,18 +13,42 @@
 // limitations under the License.
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Servlet that returns HTML recommends a music album, for now is just a test". **/
 @WebServlet("/album-recommendation")
 public class AlbumRecommendationServlet extends HttpServlet {
 
+  //stores a list of albums that will be returned
+  private ArrayList<String> albumList = new ArrayList<String>();
+
+  public AlbumRecommendationServlet() {
+    albumList.add("(1994) Definitely Maybe, by Oasis");
+    albumList.add("(2019) South of Reality, by The Claypool Lennon Delirium");
+    albumList.add("(2015) Currents, by Tame Impala");
+    albumList.add("(1971) Master of Reality, by Black Sabbath");
+    albumList.add("(1967) Axis: Bold as Love, by The Jimi Hendrix Experience");
+    albumList.add("(1971) Meddle, by Pink Floyd");
+    albumList.add("(1995) (What's the Story) Morning Glory?, by Oasis");
+    albumList.add("(1961) Led Zeppelin I, by Led Zeppelin");
+    albumList.add("(2006) Erotic Cakes, by Guthrie Govan");
+    albumList.add("(2010) Brothers, by The Black Keys");
+    albumList.add("(1972) Thick as a Brick, by Jethro Tull");
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello world!!!");
+    //converts albumList to json
+    Gson gson = new Gson();
+    String json = gson.toJson(albumList);
+
+    //returns the json
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
